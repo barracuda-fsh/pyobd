5474e45c5924d466e7ba09bbb0bb95cfbff93058
barracuda-fsh <jpoljsak@gmail.com>
2023-09-16 10:27:43 +0200

    Add files via upload

--- pyobd.py
+++ pyobd.py
@@ -1586,7 +1586,7 @@ class MyApp(wx.App):
                 self.BAUDRATE = "AUTO"
                 self.FAST = "FAST"
 
-        self.frame = wx.Frame(None, -1, "pyOBD-II ver. 1.14")
+        self.frame = wx.Frame(None, -1, "pyOBD-II ver. 1.15")
         ico = wx.Icon(resource_path('pyobd.ico'), wx.BITMAP_TYPE_ICO)
         self.frame.SetIcon(ico)
 
